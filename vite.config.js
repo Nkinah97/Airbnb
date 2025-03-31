6656752a410f31a0810779cfe3dfd83b5fda0e98
Nazar <nkinah93@gmail.com>
2025-03-31 18:02:09 +0300

    Fix file paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Airbnb/'
+  base: '/Airbnb/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
